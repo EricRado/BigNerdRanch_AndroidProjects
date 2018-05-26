e542164a3af460861e7380ddc652584e3690785a
Eric Rado <ericvictor14@hotmail.com>
2018-05-26 12:43:13 -0400

    Added generic Activity Fragment
    Singleton added for Crimes

---
+++ CriminalIntent/app/src/main/java/com/big_nerd_ranch_projects/eric/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.big_nerd_ranch_projects.eric.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ CriminalIntent/app/src/main/java/com/big_nerd_ranch_projects/eric/criminalintent/CrimeListFragment.java
@@ -0,0 +1,6 @@
+package com.big_nerd_ranch_projects.eric.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+}
